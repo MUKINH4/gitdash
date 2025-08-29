f0483af5d49a442eb14d3fc4740d143c48d9dd15
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-29 11:14:51 -0300

    terminado

---
+++ src/main/java/br/com/fiap/gitdash/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package br.com.fiap.gitdash.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain fiterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(oauth ->
+                        oauth.loginPage("/login")
+                                .defaultSuccessUrl("/")
+                                .permitAll()
+
+                )
+                .build();
+    }
+
+}
--- src/main/java/br/com/fiap/gitdash/github/GitHubController.java
+++ src/main/java/br/com/fiap/gitdash/github/GitHubController.java
@@ -1,5 +1,7 @@
 package br.com.fiap.gitdash.github;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +20,29 @@ public class GitHubController {
     }
 
     @GetMapping("/")
-    public String getUserInfo(Model model, @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient) {
+    public String getUserInfo(Model model, @RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient, @AuthenticationPrincipal OAuth2User principal) {
 
         String tokenValue = authorizedClient.getAccessToken().getTokenValue();
         List<RepositoryInfo> repos = gitHubService.getUserRepositories(tokenValue);
+        String username = principal.getName();
+        String avatarUrl = principal.getAttribute("avatar_url");
+        String htmlUrl = principal.getAttribute("html_url");
 
         model.addAttribute("repos", repos);
+        model.addAttribute("name", username);
+        model.addAttribute("html_url", htmlUrl);
+        model.addAttribute("avatar_url", avatarUrl);
 
         return "user";
+    }
+
+    @GetMapping("/login")
+    public String index() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "logout";
     }
 }
